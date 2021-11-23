49ebcca122a78d4b098dde991f971fb7e7d198f5
jmarcos00312 <jeremiahmarcos27@outlook.com>
2021-11-22 21:32:27 -0600

    commit tree a3b6eeb2c739e9d09074cb4d6aee1905d1dec0d5

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer 
+puts "Hello, programmer"
+end
+
+
+
+def greet (name = "programmer")
+puts "hello, #{name}"
+end
+
+
+def add (num1, num2)
+    puts num1 + num2
+end
+
+
+
+def halve (num1)
+    if num1.class != Integer
+        return nil
+    end
+    puts num1/2
+end
+
+greet_programmer
+greet("jeremiah")
+
+add(2,3)
+halve("10")
